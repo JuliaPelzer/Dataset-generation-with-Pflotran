922f89aa797a4de4d9e4b150524308549958d255
ac130513 <julia.pelzer@ipvs.uni-stuttgart.de>
2022-09-08 20:25:23 +0200

    starting on perm field variations, works: creation of a field, working on variation

---
+++ test_perm_field/script_pflotran_in_file_generation.py
@@ -0,0 +1,26 @@
+# vary grad(p)
+
+import logging
+import sys
+
+if __name__ == "__main__":
+      # parameters
+      skip = 1
+      file_pressure_grad = 0 #TODO
+
+      # read input parameters
+      cla_args = sys.argv
+
+      # how much output should be printed
+      logging.basicConfig(level = cla_args[1])
+      #logging.info(f"input arguments: {cla_args}")
+
+      # test varying pressure gradient, for one heat pump
+      pressure_gradient_x = 0
+      pressure_gradient_y = cla_args[2]
+      pressure_gradient_z = 0
+
+      pressure_text = f"    PRESSURE {pressure_gradient_x} {pressure_gradient_y} {pressure_gradient_z}"
+      with open("pressure_gradient.txt","w") as file:
+        file.write(pressure_text)
+      logging.info(f"Pressure Input: {pressure_gradient_x}, {pressure_gradient_y}, {pressure_gradient_z}")
