e126b92b04dc2a6186cdbec023d489b7e9f82966
ac130513 <julia.pelzer@ipvs.uni-stuttgart.de>
2022-11-02 10:11:54 +0100

    calc_pressure_variation: added pressure_min, _max values from Fabian in comment

--- scripts/scripts_pressure/script_calc_pressure_variation.py
+++ scripts/scripts_pressure/script_calc_pressure_variation.py
@@ -46,8 +46,8 @@ def sampling_regular_log(param_dataset_size):
 
 def sampling_regular_uniform(param_dataset_size):
     # regular spacing: uniform
-    value_start = -1*10**-6 
-    value_stop = -2.5*10**-4
+    value_start = -1*10**-6 #-0.001 #
+    value_stop = -2.5*10**-4 #-0.005 #fabian: -0.01 #
     x_array = np.linspace(value_start, value_stop, param_dataset_size)
     return x_array
   
