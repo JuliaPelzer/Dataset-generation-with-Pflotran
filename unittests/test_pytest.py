3f3ae723121c2c01d21119a4c42b156dcc64f2cf
ac130513 <julia.pelzer@ipvs.uni-stuttgart.de>
2022-11-07 10:48:27 +0100

    unittests: clean up dir after test

--- unittests/test_pytest.py
+++ unittests/test_pytest.py
@@ -1,17 +1,20 @@
+# run pytest from unittests folder
 import subprocess
 import os
 
 def test_run_bash_pressure_1D():
     os.getcwd()
     subprocess.call("bash ../make_dataset_2.sh 1 test_bash_1D 1D", shell=True)
-    assert _fcount("test_bash_1D") == 1, "1D created too little runs"
+    assert _fcount("test_bash_1D") == 1, "1D created too little datapoints"
     os.system("rm -r test_bash_1D")
+    os.system("rm -r __pycache__")
 
 def test_run_bash_pressure_2D():
     os.getcwd()
     subprocess.call("bash ../make_dataset_2.sh 1 test_bash_2D 2D", shell=True)
-    assert _fcount("test_bash_2D") == 4, "2D created too little runs"
+    assert _fcount("test_bash_2D") == 4, "2D created too little datapoints"
     os.system("rm -r test_bash_2D")
+    os.system("rm -r __pycache__")
 
 def _fcount(path):
   count = 0
