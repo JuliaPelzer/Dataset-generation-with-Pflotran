19d5209b82687629af55674a2d353e8557ea633e
ac130513 <julia.pelzer@ipvs.uni-stuttgart.de>
2023-01-23 16:13:10 +0100

    make.sh: option to start with random datapoint in dataset - not first one; AND default input args

--- make_dataset.sh
+++ make_dataset.sh
@@ -9,6 +9,18 @@
 
 #command line arguments
 args=("$@")
+if [ ${#args[@]} = 0 ];
+then
+    args=(10 1D 10 iso 3D default_valued_dataset)
+    echo "No command line arguments given. Using default values:"
+    echo "<Number of pressure variations>       default: ${args[0]}"
+    echo "<Pressure case> (1D / 2D)             default: ${args[1]}"
+    echo "<Number permeability variations>      default: ${args[2]}"
+    echo "<Perm case> (Iso / vary)              default: ${args[3]}"
+    echo "<Dimensions of domain> (2D / 3D)      default: ${args[4]}"
+    echo "<Name of folder for dataset>          default: ${args[5]}"
+    echo "<Visualization?> (vis / -)            default: ${args[6]}"
+fi
 CLA_NUMBER_VARIATIONS_PRESSURE=${args[0]} # expects the number of desired variations of the pressure field in the dataset
 CLA_PRESSURE_CASE=${args[1]} # expects a string: "2D" or "1D" (for the pressure field)
 CLA_NUMBER_VARIATIONS_PERMEABILITY=${args[2]} # expects the number of desired variations of the permeability field in the dataset
@@ -17,6 +29,11 @@ CLA_DIMENSIONS=${args[4]} # expects a string: "2D" or "3D"
 CLA_NAME=${args[5]} # expects a string with the name of the dataset
 CLA_VISUALISATION=${args[6]} # expects "vis" or "no_vis"
 
+if [ ! "$CLA_PRESSURE_CASE" = "1D" ] && [ ! "$CLA_PRESSURE_CASE" = "2D" ];
+then
+    echo "CLA_PRESSURE_CASE must be 1D or 2D"
+    exit 1
+fi
 if [ ! "$CLA_DIMENSIONS" = "2D" ] && [ ! "$CLA_DIMENSIONS" = "3D" ];
 then
     echo "CLA_DIMENSIONS must be 2D or 3D"
@@ -78,58 +95,61 @@ do
     # TODO random combination of pressure+permeability field or all combinations? currently: all combinations
     j=0
     while [ $j -lt $len_perm ];
-    do
-        # calculate pressure files
-        if [ "$CLA_PRESSURE_CASE" = "1D" ]; 
+    do  
+        current_id=-1 #79 # TODO set to -1 if you want to start from the beginning
+        if [ $j -gt $current_id ];
         then
-            python3 scripts/script_write_pressure_to_pflotran_in_file.py INFO ${PRESSURE[$i]}
-        else # 2D case
-            python3 scripts/script_write_pressure_to_pflotran_in_file.py INFO ${PRESSURE[$i]} ${PRESSURE[$i+1]}
-        fi
 
-        # create run folder    
-        NAME_OF_RUN="RUN_${run_id}"
-        OUTPUT_DATASET_RUN_DIR="${OUTPUT_DATASET_DIR}/${NAME_OF_RUN}"
-        OUTPUT_DATASET_RUN_PREFIX="${OUTPUT_DATASET_RUN_DIR}/pflotran"
-        # check whether output folder exists else define
-        if [ ! -d $OUTPUT_DATASET_RUN_DIR ]
-        then
-            mkdir $OUTPUT_DATASET_RUN_DIR
-        fi
+            # calculate pressure files
+            if [ "$CLA_PRESSURE_CASE" = "1D" ]; 
+            then
+                python3 scripts/script_write_pressure_to_pflotran_in_file.py INFO ${PRESSURE[$i]}
+            else # 2D case
+                python3 scripts/script_write_pressure_to_pflotran_in_file.py INFO ${PRESSURE[$i]} ${PRESSURE[$i+1]}
+            fi
 
-        # copy next permeability field file to pflotran.in folder
-        if [ "$CLA_PERM_CASE" = "vary" ]; 
-        then
-            python3 scripts/script_copy_next_perm_field.py $OUTPUT_DATASET_DIR $j $NAME_OF_RUN
+            # create run folder    
+            NAME_OF_RUN="RUN_${run_id}"
+            OUTPUT_DATASET_RUN_DIR="${OUTPUT_DATASET_DIR}/${NAME_OF_RUN}"
+            OUTPUT_DATASET_RUN_PREFIX="${OUTPUT_DATASET_RUN_DIR}/pflotran"
+            # check whether output folder exists else define
+            if [ ! -d $OUTPUT_DATASET_RUN_DIR ]
+            then
+                mkdir $OUTPUT_DATASET_RUN_DIR
+            fi
+
+            # copy next permeability field file to pflotran.in folder
+            if [ "$CLA_PERM_CASE" = "vary" ]; 
+            then
+                python3 scripts/script_copy_next_perm_field.py $OUTPUT_DATASET_DIR $j $NAME_OF_RUN
+            fi
+        
+            echo starting PFLOTRAN simulation of $NAME_OF_RUN at $(date)
+            # to DEBUG the simulation turn screen_output on
+            # mpirun -n 1 $PFLOTRAN_DIR/src/pflotran/pflotran -output_prefix $OUTPUT_DATASET_RUN_PREFIX -screen_output off # local version
+            mpirun -n 16 $PFLOTRAN_DIR/pflotran -output_prefix $OUTPUT_DATASET_RUN_PREFIX -screen_output off # remote version (pcsgs05)
+            echo finished PFLOTRAN simulation at $(date)
+
+            cp interim_pressure_gradient.txt $OUTPUT_DATASET_RUN_DIR/pressure_gradient.txt
+
+            # # call visualisation
+            if [ "$CLA_VISUALISATION" = "vis" ]; 
+            then
+                if [ "$CLA_DIMENSIONS" = "2D" ];
+                then
+                    python3 scripts/visualisation_self.py $OUTPUT_DATASET_DIR $OUTPUT_DATASET_RUN_DIR "2D"
+                else
+                    python3 scripts/visualisation_self.py $OUTPUT_DATASET_DIR $OUTPUT_DATASET_RUN_DIR
+                    python3 scripts/visualisation_self.py $OUTPUT_DATASET_DIR $OUTPUT_DATASET_RUN_DIR "top_hp"
+                fi
+                echo ...visualisation of $NAME_OF_RUN is done
+            fi
         fi
         j=$(( $j + 1 ))
-    
-        echo starting PFLOTRAN simulation of $NAME_OF_RUN at $(date)
-        # to DEBUG the simulation turn screen_output on
-        mpirun -n 1 $PFLOTRAN_DIR/src/pflotran/pflotran -output_prefix $OUTPUT_DATASET_RUN_PREFIX -screen_output off
-        echo finished PFLOTRAN simulation at $(date)
-
-        cp interim_pressure_gradient.txt $OUTPUT_DATASET_RUN_DIR/pressure_gradient.txt
-
-        # # call visualisation
-        if [ "$CLA_VISUALISATION" = "vis" ]; 
-        then
-            if [ "$CLA_DIMENSIONS" = "2D" ];
-            then
-                python3 scripts/visualisation_self.py $OUTPUT_DATASET_DIR $OUTPUT_DATASET_RUN_DIR "2D"
-            else
-                python3 scripts/visualisation_self.py $OUTPUT_DATASET_DIR $OUTPUT_DATASET_RUN_DIR
-                python3 scripts/visualisation_self.py $OUTPUT_DATASET_DIR $OUTPUT_DATASET_RUN_DIR "top_hp"
-            fi
-            echo ...visualisation of $NAME_OF_RUN is done
-        fi
-
         run_id=$(( $run_id + 1 ))
     done
-    # pressure consists of two values, so only half of the number of pressure values in loop
     i=$(( $i + 1 ))
 done
-
 
 mv pflotran.in $OUTPUT_DATASET_DIR/inputs/pflotran_copy.in
 rm interim_pressure_gradient.txt
