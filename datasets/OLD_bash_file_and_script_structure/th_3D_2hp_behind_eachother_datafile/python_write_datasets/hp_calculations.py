3cb4833cfe748826c057817f25393fd446de7b87
Julia Pelzer <pelzerja@ipvs.uni-stuttgart.de>
2023-03-13 13:26:11 +0100

    removed OLD_bash_file_and_script_structure

--- datasets/OLD_bash_file_and_script_structure/th_3D_2hp_behind_eachother_datafile/python_write_datasets/hp_calculations.py
+++
@@ -1,4 +0,0 @@
-# define mass rate from 
-# Pumpleistung heat pump (volumetric rate 1.800 l/h = 150 l/h/kW * 12kW): https://www.energieheld.ch/heizung/waermepumpe/wasser-wasser (28.03.2022)
-# 1.800 l/h = 1.8 m^3 /h = 43.2 m^3/day
-# wenn mit Piekleistung die ganze Zeit voll durchpowern
