1ecf1adb376c2023a743e0a951f69c28b4a71fd5
JuliaPelzer <jupelzer@outlook.de>
2023-06-13 10:33:41 +0200

    new: make pipeline: option to specify NUM of hps to vary vs. those to be on a fixed position

--- make_dataset_pipeline_large.py
+++ make_dataset_pipeline_large.py
@@ -20,6 +20,7 @@ def run_simulation(args):
         args.num_dp = 1
         args.visu = True
         args.vary_hp = True
+        args.vary_hp_amount = 0
         args.num_hps = 2
         args.vary_perm = False #True
         logging.info(f"Running benchmark testcases with settings: {args}")
@@ -47,7 +48,7 @@ def run_simulation(args):
 
     # copy settings file
     shutil.copy("dummy_dataset_pipeline_large/settings_2D_large.yaml", f"{output_dataset_dir}/inputs/settings.yaml")
-    if args.benchmark:
+    if args.benchmark or (args.num_hps - args.vary_hp_amount > 0):
         shutil.copy("dummy_dataset_pipeline_large/benchmark_locs_hps.yaml", f"{output_dataset_dir}/inputs/benchmark_locs_hps.yaml")
 
     # copy pflotran.in file, which one - depends
@@ -64,7 +65,7 @@ def run_simulation(args):
 
     # potentially calc 1 or 2 hp locations
     if args.vary_hp:
-        locs_hps = calc_loc_hp_variation_2d(args.num_dp, f"{output_dataset_dir}/inputs", args.num_hps, settings, benchmark_bool=args.benchmark)
+        locs_hps = calc_loc_hp_variation_2d(args.num_dp, f"{output_dataset_dir}/inputs", args.num_hps, settings, benchmark_bool=args.benchmark, num_hps_to_vary = args.vary_hp_amount)
     
     # make benchmark testcases
     pressures, perms = calc_pressure_and_perm_fields(args.num_dp, f"{output_dataset_dir}/inputs", args.vary_perm, benchmark_bool=args.benchmark)
@@ -111,6 +112,12 @@ def run_simulation(args):
             continue
     if args.vary_perm:
         shutil.rmtree(f"{output_dataset_dir}/permeability_fields")
+    if not args.benchmark:
+        try:
+            os.remove(f"{output_dataset_dir}/inputs/benchmark_locs_hps.yaml")
+        except:
+            pass
+
 
 def just_visualize(args):
     output_dataset_dir = args.name
@@ -130,8 +137,9 @@ if __name__ == "__main__":
     parser.add_argument("--num_dp", type=int, default=1) #int = 100 # number of datapoints
     parser.add_argument("--name", type=str, default="default") #benchmark_large_vary_perm_2hps")
     parser.add_argument("--visu", type=bool, default=False) # visualisation
-    parser.add_argument("--vary_hp", type=bool, default=False)  # vary hp location
     parser.add_argument("--num_hps", type=int, default=0)   # number of hp locations
+    parser.add_argument("--vary_hp", type=bool, default=False)  # vary hp location
+    parser.add_argument("--vary_hp_amount", type=int, default=0) # how many hp locations should be varied
     parser.add_argument("--vary_perm", type=bool, default=False)    # vary permeability
 
     args = parser.parse_args()
--- scripts/calc_loc_hp_variation_2d.py
+++ scripts/calc_loc_hp_variation_2d.py
@@ -7,9 +7,12 @@ try:
 except:
     import make_general_settings as mgs
 
-def calc_loc_hp_variation_2d(param_dataset_size: int, dataset_folder: str, number_of_hps: int, settings, benchmark_bool:bool = False):
+def calc_loc_hp_variation_2d(param_dataset_size: int, dataset_folder: str, number_of_hps: int, settings, benchmark_bool:bool = False, num_hps_to_vary: int = 0):
 
     assert number_of_hps > 0, "no clue what happens if number of hps is 0"
+    assert num_hps_to_vary <= number_of_hps, "number of hps to vary must be smaller than number of hps"
+
+    # write input lines for pflotran for number of hps
     with open("regions_hps.txt", "w") as f:
         for hp in range(number_of_hps):
             f.write(f"REGION heatpump_inject{hp}\n  FILE heatpump_inject{hp}.vs\nEND\n\n")
@@ -25,19 +28,24 @@ def calc_loc_hp_variation_2d(param_dataset_size: int, dataset_folder: str, numbe
     grid_size = settings["grid"]["size"]
     cell_size = settings["grid"]["size"]/np.array(settings["grid"]["ncells"])
 
-    if benchmark_bool:
+    if benchmark_bool or (number_of_hps - num_hps_to_vary > 0):
         hps = mgs.load_yaml(dataset_folder, file_name="benchmark_locs_hps")
-        for i in range(number_of_hps):
+        for i in range(number_of_hps - num_hps_to_vary):
             locs_hps[i] = np.array(hps[i+1])
 
-    else:
+            if not benchmark_bool:
+                # save to file
+                with open(os.path.join(dataset_folder, f"locs_hp_{i+1}_fixed.txt"), "w") as f:
+                    np.savetxt(f, locs_hps[i])
+
+    if not benchmark_bool:
         try:
             distance_to_border_in_cells = settings["grid"]["distance_to_border_in_cells"]
         except:
             distance_to_border_in_cells = 5
         distance_to_border = distance_to_border_in_cells*cell_size
 
-        for i in range(number_of_hps):
+        for i in range(number_of_hps - num_hps_to_vary, number_of_hps):
             # choose random position inside domain
             locs_x = np.random.randint(0+distance_to_border[0], grid_size[0]-distance_to_border[0], param_dataset_size)
             locs_y = np.random.randint(0+distance_to_border[1], grid_size[1]-distance_to_border[1], param_dataset_size)
