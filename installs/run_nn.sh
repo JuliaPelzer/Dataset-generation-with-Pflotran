a31f3f2c64e7eff1af2692f15e0ddd4b85f49555
JuliaPelzer <jupelzer@outlook.de>
2024-04-03 17:18:50 +0200

    clean up: rm install folder

--- installs/run_nn.sh
+++
@@ -1,6 +0,0 @@
-pyenv activate test_nn
-module load cuda-11.2.1
-cd ~/test_nn/3D_FCN/
-python3 /home/pelzerja/pelzerja/test_nn/3D_FCN/main_remote.py 1
-
-# nohup damit es weiterläuft, wenn ich die verbindung trenne
