499de70a11418a1f66cf0b9307e04a39fa9d9a6c
JuliaPelzer <jupelzer@outlook.de>
2025-01-24 16:36:14 +0100

    improve hp-variation :work with diverse inputs of distance-to-border

--- scripts/hp_variation_2d.py
+++ scripts/hp_variation_2d.py
@@ -12,52 +12,6 @@ def write_hps_strata_conditions_files(dataset_folder_interim: str, number_of_hps
         for hp in range(number_of_hps):
             f.write(f"SOURCE_SINK heatpump_inject{hp}\n  FLOW_CONDITION injection{hp}\n  REGION heatpump_inject{hp}\nEND\n\n")
     
-# def calc_hp_locs(vary_poss: bool, num_dps: int, num_hps: int, settings: Dict):
-#     """
-#     Outputs:
-#         hps_locs: np.ndarray, shape (num_dps, num_hps, 3), locations of heat pumps in meters
-#     """
-    
-#     # get boundaries of domain
-#     grid_size = settings["grid"]["size [m]"]
-    
-#     if vary_poss:
-#         try:
-#             distance_to_border = settings["grid"]["distance_to_border"]
-#             print(f"distance to border: {distance_to_border} m, {len(distance_to_border[1])} values")
-#         except:
-#             distance_to_border = [[5], [5,5], 5]
-#         assert len(distance_to_border[1]) == 1 or len(distance_to_border[1]) == 2, "distance to border in y direction must be either one value or two values"
-
-#     # choose random position inside domain
-#     # TODO better variation, TODO float instead of int for position TODO 3D
-#     if vary_poss:
-#         try:
-#             locs_x = np.random.randint(0 + distance_to_border[0][0],grid_size[0] - distance_to_border[0][1],num_dps*num_hps,)
-#         except:
-#             try:
-#                 locs_x = np.random.randint(0 + distance_to_border[0][0],grid_size[0] - distance_to_border[0][0],num_dps*num_hps,)
-#             except:
-#                 locs_x = np.random.randint(0 + distance_to_border,grid_size[0] - distance_to_border,num_dps*num_hps,)
-
-#         if len(distance_to_border[1]) == 1:
-#             locs_y = np.random.randint(0 + distance_to_border[1],grid_size[1] - distance_to_border[1],num_dps*num_hps,)
-#         elif len(distance_to_border[1]) == 2:
-#             locs_y = np.random.randint(distance_to_border[1][0],grid_size[1] - distance_to_border[1][1],num_dps*num_hps,)
-
-#         hps_locs = np.array([locs_x, locs_y,np.ones_like(locs_x)]).T
-
-#     else:
-#         try:
-#             hps_locs = [settings["grid"]["loc_hp [m]"]]
-
-#         except:
-#             # if nothing specified: center position
-#             hps_locs = [list((np.array(settings["grid"]["size [m]"])/2).astype(int))]
-#     hps_locs = hps_locs.reshape((num_hps, num_dps, 3))
-#     hps_locs = np.swapaxes(hps_locs, 0, 1)
-#     return hps_locs
-
 def hps_locs_to_ids(hps_locs: np.ndarray, meshs_refined: np.ndarray):
     """
     Convert locations of heat pumps to cell ids
@@ -86,32 +40,31 @@ def calc_hps_locs_float(vary_poss: bool, num_dps: int, num_hps: int, settings: D
     # get boundaries of domain
     grid_size = settings["grid"]["size [m]"]
     
-    if vary_poss:
-        try:
-            distance_to_border = settings["grid"]["distance_to_border"]
-            logging.info(f"distance to border: {distance_to_border} m, {len(distance_to_border[1])} values")
-        except:
-            distance_to_border = [[5], [5,5], 5]
-        assert len(distance_to_border[1]) == 1 or len(distance_to_border[1]) == 2, "distance to border in y direction must be either one value or two values"
-
     # choose random position inside domain
-    # TODO better variation
-    # TODO 3D
     if vary_poss:
-        try:
-            locs_x = np.random.uniform(0 + distance_to_border[0][0],grid_size[0] - distance_to_border[0][1],num_dps*num_hps,)
-        except:
+        if isinstance(settings["grid"]["distance_to_border"], int):
+            distance_to_border = [settings["grid"]["distance_to_border"], settings["grid"]["distance_to_border"], settings["grid"]["distance_to_border"]]
+        else:
             try:
-                locs_x = np.random.uniform(0 + distance_to_border[0][0],grid_size[0] - distance_to_border[0][0],num_dps*num_hps,)
+                distance_to_border = settings["grid"]["distance_to_border"]
+                logging.info(f"distance to border: {distance_to_border} m values")
             except:
-                locs_x = np.random.uniform(0 + distance_to_border,grid_size[0] - distance_to_border,num_dps*num_hps,)
+                distance_to_border = [[5], [5,5], 5]
+        assert len(distance_to_border) in [2,3], "distance_to_border must have 2 or 3 values"
+    # TODO 3D
+        try:
+            dists_x = distance_to_border[0][0], distance_to_border[0][1]
+        except:
+            dists_x = distance_to_border[0], distance_to_border[0]
+        locs_x = np.random.uniform(0 + dists_x[0],grid_size[0] - dists_x[1],num_dps*num_hps,)
 
-        if len(distance_to_border[1]) == 1:
-            locs_y = np.random.uniform(0 + distance_to_border[1],grid_size[1] - distance_to_border[1],num_dps*num_hps,)
-        elif len(distance_to_border[1]) == 2:
-            locs_y = np.random.uniform(distance_to_border[1][0],grid_size[1] - distance_to_border[1][1],num_dps*num_hps,)
+        try:
+            dists_y = distance_to_border[1][0], distance_to_border[1][1]
+        except:
+            dists_y = distance_to_border[1], distance_to_border[1]
+        locs_y = np.random.uniform(0 + dists_y[0],grid_size[1] - dists_y[1],num_dps*num_hps,)
 
-        hps_locs = np.array([locs_x, locs_y,np.ones_like(locs_x)]).T
+        hps_locs = np.array([locs_x, locs_y,np.ones_like(locs_x)]).T # TODO if 3D
 
     else:
         try:
