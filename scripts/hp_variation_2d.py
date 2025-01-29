1d1d109764905f888d96c3fc35a802b27130f493
JuliaPelzer <jupelzer@outlook.de>
2025-01-29 10:28:04 +0100

    hp variation: 3D, more robust regarding distance-to-border vs. grid_size/2

--- scripts/hp_variation_2d.py
+++ scripts/hp_variation_2d.py
@@ -39,40 +39,56 @@ def calc_hps_locs_float(vary_poss: bool, num_dps: int, num_hps: int, settings: D
     
     # get boundaries of domain
     grid_size = settings["grid"]["size [m]"]
+    res = settings["grid"]["resolution"]
+
+    # expand 3rd dimension if not given
+    if len(grid_size) == 2:
+        grid_size.append(settings["grid"]["resolution"])
     
     # choose random position inside domain
     if vary_poss:
+
+        # unify format of distance to border        
         if isinstance(settings["grid"]["distance_to_border"], int):
             distance_to_border = [settings["grid"]["distance_to_border"], settings["grid"]["distance_to_border"], settings["grid"]["distance_to_border"]]
         else:
-            try:
+            if settings["grid"]["distance_to_border"] is not None:
                 distance_to_border = settings["grid"]["distance_to_border"]
                 logging.info(f"distance to border: {distance_to_border} m values")
-            except:
-                distance_to_border = [[5], [5,5], 5]
+            else:
+                distance_to_border = [res, res, res/2-1]
         assert len(distance_to_border) in [2,3], "distance_to_border must have 2 or 3 values"
-    # TODO 3D
+
         try:
-            dists_x = distance_to_border[0][0], distance_to_border[0][1]
+            dists_x = [distance_to_border[0][0], distance_to_border[0][1]]
         except:
-            dists_x = distance_to_border[0], distance_to_border[0]
+            dists_x = [distance_to_border[0], distance_to_border[0]]
+        try:
+            dists_y = [distance_to_border[1][0], distance_to_border[1][1]]
+        except:
+            dists_y = [distance_to_border[1], distance_to_border[1]]
+        dists_z = [distance_to_border[2], ]*2
+
+        # ensure that distance to both dit-to-borders together are smaller than domain size in that dimension
+        for dist, size in zip([dists_x, dists_y, dists_z], grid_size):
+            dist_sum = np.sum(dist)
+            if dist_sum >= size:
+                logging.warning(f"distance to border ({dist_sum}) must be smaller than domain size ({size})")
+                for id, _ in enumerate(dist):
+                    dist[id] = size/2 - 1
+
         locs_x = np.random.uniform(0 + dists_x[0],grid_size[0] - dists_x[1],num_dps*num_hps,)
-
-        try:
-            dists_y = distance_to_border[1][0], distance_to_border[1][1]
-        except:
-            dists_y = distance_to_border[1], distance_to_border[1]
         locs_y = np.random.uniform(0 + dists_y[0],grid_size[1] - dists_y[1],num_dps*num_hps,)
+        locs_z = np.random.uniform(0 + dists_z[0], grid_size[2] - dists_z[1], num_dps*num_hps,)
 
-        hps_locs = np.array([locs_x, locs_y,np.ones_like(locs_x)]).T # TODO if 3D
-
+        hps_locs = np.array([locs_x, locs_y, locs_z]).T # np.ones_like(locs_x)]).T
     else:
         try:
             hps_locs = [settings["grid"]["loc_hp [m]"]]
 
         except:
             # if nothing specified: center position
-            hps_locs = [list((np.array(settings["grid"]["size [m]"])/2))]
+            hps_locs = [list((np.array(grid_size)/2))]
 
     hps_locs = hps_locs.reshape((num_hps, num_dps, 3))
     hps_locs = np.swapaxes(hps_locs, 0, 1)
